26e6851e17ecc04d3b015aca11b4e321ef92a493
Nick Taylor <nick@fractaldimensions.io>
2020-01-09 21:53:30 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+const url = 'https://www.racingpost.com';
+const fs = require('fs');
+
+puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => page.goto(url).then(() => page.content()))
+    .then(html => fs.writeFile("./data/result.html", html, (err) => console.log(err)))
+    .then(() => console.log("COMPLETE!"))
+    .catch(err => console.log(err));
